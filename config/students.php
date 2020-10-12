47422614f59540f3cab8c99b1b183671ab5a4c4a
Alex <gurievalex78@gmail.com>
2020-10-12 15:11:16 +0200

    students

---
+++ config/students.php
@@ -0,0 +1,19 @@
+<?php 
+return [
+    [
+        'img' => 'https://www.boolean.careers/images/people/ortenzi.png',
+        'nome'=> 'Daniela Ortenzi',
+        'anni'=> '27',
+        'azienda' => 'RDS Radio',
+        'ruola'=> 'web developer',
+        'descrizione' => 'Laureata in psicologia alla Sapienza di Roma con una nascente carriere nella SEO, Daniela si iscrive al corso Boolean. Ora lavora come web developer presso RDS Radio Dimensione Suono.'
+    ],
+    [
+        'img' => 'https://www.boolean.careers/images/people/antidormi.png',
+        'nome' => 'Nicholas Antidormi',
+        'anni' => '20',
+        'azienda' => 'ATOMS',
+        'ruola' => 'web developer',
+        'descrizione' => 'Intraprende gli studi universitari in matematica per le sue doti analitiche, ma abbandona il percorso per concentrarsi su uno più pratico e orientato alla richiesta del mercato del lavoro: la programmazione.'
+    ]
+    ];
