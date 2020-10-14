a4cc01c5bb1634d3d4e374402090a1d0fb63706a
Alex <gurievalex78@gmail.com>
2020-10-14 17:46:54 +0200

    studenti e show

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,16 +3,25 @@
 @section('titolo','Student')
     
 @section('content')
-    <div class="card-group">
-        
-            <div class="card">
-            <img src="{{$student['img']}}" class="card-img-top" alt="{{$student['nome']}}">
-                <div class="card-body">
-                <h5 class="card-title text-green">{{$student['nome']}}</h5>
-                <h6>Assunt{{($student['genere']=='F') ? 'a':'o'}} da {{$student['azienda']}}</h6>
-                <p class="card-text">{{$student['descrizione']}}</p> 
-                <p class="card-text"><small class="text-muted">{{$student['ruolo']}}</small></p>
+<div class="row">
+        <div class="col-12">
+                <div class="wrapper-container-scroll">
+                    <ul class="container-scroll center">
+                        <li class="boolean__card container-scroll__element">
+                            <div class="card__top">
+                                <img  src="{{$student['img']}}" alt="{{$student['nome']}}">
+                                <div>
+                                    <h5 class="text-paragraph text-green">{{$student['nome']}}</h5>
+                                    <strong class="text-paragraph">Assunt{{($student['genere']=='F') ? 'a':'o'}} da {{$student['azienda']}}</strong>
+                                </div>
+                            </div>
+                            <div class="card__bottom">
+                                <p>{{$student['descrizione']}} </p>
+                                <a href="https://www.linkedin.com/in/daniela-ortenzi/" target="_blank"><img src="https://www.boolean.careers/images/socials/linkedin-2.png" alt=""></a>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
-            </div>
+        </div>
 </div>
 @endsection
--- resources/views/studenti.blade.php
+++ resources/views/studenti.blade.php
@@ -1,19 +1,30 @@
 @extends('layouts.app')
 
 @section('titolo','Student')
-    
+
 @section('content')
-    <div class="card-group">
-        @foreach ($data as $key =>$student) 
-            <div class="card">
-            <img src="{{$student['img']}}" class="card-img-top" alt="{{$student['nome']}}">
-                <div class="card-body">
-                <a href="{{route('Student.show',['id'=>$key])}}"><h5 class="card-title text-green">{{$student['nome']}}</h5></a>
-                <h6>Assunt{{($student['genere']=='F') ? 'a':'o'}} da {{$student['azienda']}}</h6>
-                {{-- <p class="card-text">{{$student['descrizione']}}</p> --}}
-                <p class="card-text"><small class="text-muted">{{$student['ruolo']}}</small></p>
+<div class="row">
+        <div class="col-12">
+                <div class="wrapper-container-scroll">
+                    <ul class="container-scroll">
+                        @foreach ($data as $key =>$student) 
+                        <li class="boolean__card container-scroll__element">
+                            <div class="card__top fl-container">
+                                <img  src="{{$student['img']}}" alt="{{$student['nome']}}">
+                                <div>
+                                    <a href="{{route('Student.show',['id'=>$key])}}"><h5 class="text-paragraph text-green">{{$student['nome']}}</h5></a>
+                                    <strong class="text-paragraph t">Assunt{{($student['genere']=='F') ? 'a':'o'}} da {{$student['azienda']}}</strong>
+                                </div>
+                            </div>
+                            <div class="card__bottom">
+                                <p>{{$student['descrizione']}} </p>
+                                <a href="https://www.linkedin.com/in/daniela-ortenzi/" target="_blank"><img src="https://www.boolean.careers/images/socials/linkedin-2.png" alt=""></a>
+                            </div>
+                        </li>
+                        @endforeach
+                    </ul>
                 </div>
-            </div>
-        @endforeach
+        </div>
 </div>
 @endsection
+
