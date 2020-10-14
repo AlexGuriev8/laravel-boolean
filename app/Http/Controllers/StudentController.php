29370ef98c6691ba2018d15379dd37b713e50e9c
Alex <gurievalex78@gmail.com>
2020-10-14 09:33:15 +0200

    student controller

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,8 +6,23 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
+    protected $students;
+
+    public function __construct()
+    {
+        $this->students = config('students');
+    }
+
     public function studenti(){
-        $data = config('students');
+        $data = $this->students;
         return view('studenti',compact('data'));
+    }
+
+    public function show($id){
+        if(!array_key_exists($id,$this->students)){
+            abort(404);
+        }
+        $student = $this->students[$id];
+       return  view('show',compact('student'));
     }
 }
