764dc4b78a52fdd78b179b209d131765d30a98a0
Alex <gurievalex78@gmail.com>
2020-10-15 17:26:25 +0200

    adj migration

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -9,14 +9,14 @@
                     <ul class="container-scroll center">
                         <li class="boolean__card container-scroll__element">
                             <div class="card__top">
-                                <img  src="{{$student['img']}}" alt="{{$student['nome']}}">
+                                <img  src="{{$student->img}}" alt="{{$student->nome}}">
                                 <div>
-                                    <h5 class="text-paragraph text-green">{{$student['nome']}}</h5>
-                                    <strong class="text-paragraph">Assunt{{($student['genere']=='F') ? 'a':'o'}} da {{$student['azienda']}}</strong>
+                                    <h5 class="text-paragraph text-green">{{$student->nome}}</h5>
+                                    <strong class="text-paragraph">Assunt{{($student->genere=='F') ? 'a':'o'}} da {{$student->azienda}}</strong>
                                 </div>
                             </div>
                             <div class="card__bottom">
-                                <p>{{$student['descrizione']}} </p>
+                                <p>{{$student->descrizione}} </p>
                                 <a href="https://www.linkedin.com/in/daniela-ortenzi/" target="_blank"><img src="https://www.boolean.careers/images/socials/linkedin-2.png" alt=""></a>
                             </div>
                         </li>
--- resources/views/studenti.blade.php
+++ resources/views/studenti.blade.php
@@ -10,14 +10,14 @@
                         @foreach ($data as $key =>$student) 
                         <li class="boolean__card container-scroll__element">
                             <div class="card__top fl-container">
-                                <img  src="{{$student['img']}}" alt="{{$student['nome']}}">
+                                 <img  src="{{$student->img}}" alt="{{$student->nome}}"> 
                                 <div>
-                                    <a href="{{route('Student.show',['id'=>$key])}}"><h5 class="text-paragraph text-green">{{$student['nome']}}</h5></a>
-                                    <strong class="text-paragraph t">Assunt{{($student['genere']=='F') ? 'a':'o'}} da {{$student['azienda']}}</strong>
+                                    <a href="{{route('Student.show',['id'=>$student->id])}}"><h5 class="text-paragraph text-green">{{$student->nome}}</h5></a>
+                                    <strong class="text-paragraph t">Assunt{{($student->genere) =='F' ? 'a':'o'}} da {{$student->azienda}}</strong>
                                 </div>
                             </div>
                             <div class="card__bottom">
-                                <p>{{$student['descrizione']}} </p>
+                                <p>{{$student->descrizione}} </p>
                                 <a href="https://www.linkedin.com/in/daniela-ortenzi/" target="_blank"><img src="https://www.boolean.careers/images/socials/linkedin-2.png" alt=""></a>
                             </div>
                         </li>
